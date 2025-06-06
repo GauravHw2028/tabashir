b5059640c4cdb7e224fcbdd38755324e9843dd3e
Mohid Faizi <mohidfaizipro@gmail.com>
2025-06-06 09:30:17 +0500

    Fixing the session issue with the sessionprovider

--- 111/tabashir_hr_consulting/app/(candidate)/layout.tsx
+++ 111/tabashir_hr_consulting/app/(candidate)/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import { auth } from "../utils/auth"
 import { redirect } from "next/navigation"
 import SidebarLayout from "./dashboard/_components/sidebar-layout";
+import { SessionProvider } from "next-auth/react";
 
 export default async function CandidateLayout({
   children,
@@ -14,11 +15,13 @@ export default async function CandidateLayout({
     redirect("/candidate/login")
   }
   return (
-    <div className="flex bg-[#F0F0F0] p-4 relative">
-      <SidebarLayout />
-      <div className="flex-1 flex flex-col">
-        <main className="pl-4 lg:pl-4 lg:pt-0 flex-1">{children}</main>
+    <SessionProvider>
+      <div className="flex bg-[#F0F0F0] p-4 relative">
+        <SidebarLayout />
+        <div className="flex-1 flex flex-col">
+          <main className="pl-4 lg:pl-4 lg:pt-0 flex-1">{children}</main>
+        </div>
       </div>
-    </div>
+    </SessionProvider>
   )
 }
