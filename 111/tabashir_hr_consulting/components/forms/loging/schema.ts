fb678e865c2f27dc447ab964cf134915fb2f6075
Mohid Faizi <mohidfaizipro@gmail.com>
2025-06-09 22:30:07 +0500

    Making the password a little easy to create

--- 111/tabashir_hr_consulting/components/forms/loging/schema.ts
+++ 111/tabashir_hr_consulting/components/forms/loging/schema.ts
@@ -4,8 +4,8 @@ export const candidateLoginFormSchema = z.object({
     email: z.string().email({
         message: "Please enter a valid email address.",
     }),
-    password: z.string().min(8, {
-        message: "Password must be at least 8 characters.",
+    password: z.string().min(5, {
+        message: "Password must be at least 5 characters.",
     }),
 });
 
--- 111/tabashir_hr_consulting/components/forms/registration/candidate/schema.ts
+++ 111/tabashir_hr_consulting/components/forms/registration/candidate/schema.ts
@@ -7,10 +7,8 @@ export const registrationFormSchema = z.object({
     username: z.string().min(2, {
       message: "Username must be at least 2 characters.",
     }),
-    password: z.string().min(8, {
-      message: "Password must be at least 8 characters.",
-    }).regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
-      message: "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character.",
+    password: z.string().min(5, {
+      message: "Password must be at least 5 characters.",
     }),
   })
   
