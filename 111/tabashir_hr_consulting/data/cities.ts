431403c13331f6a09ef19d422951511f033382bf
Mohid Faizi <mohidfaizipro@gmail.com>
2025-06-24 12:38:35 +0500

    Fixing the cities

--- 111/tabashir_hr_consulting/data/cities.ts
+++ 111/tabashir_hr_consulting/data/cities.ts
@@ -1,10 +1,10 @@
 export const CITIES = [
   // UAE Cities
-  "Dubai, UAE",
-  "Abu Dhabi, UAE",
-  "Sharjah, UAE",
-  "Ajman, UAE",
-  "Ras Al Khaimah, UAE",
-  "Fujairah, UAE",
-  "Al Ain, UAE",
+  "Dubai",
+  "Abu Dhabi",
+  "Sharjah",
+  "Ajman",
+  "Ras Al Khaimah",
+  "Fujairah",
+  "Al Ain",
 ] 
